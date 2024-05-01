0be4a12c1601e9743debb56d009dd3555cc118cc
Joerg Roth <joerg.roth@geo.uzh.ch>
2024-05-01 12:11:29 +0200

    update to latest

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { beckerhagens, archimedean } from './lib/solids.mjs';
+import { beckerhagens, archimedean, platonic } from './lib/solids.mjs';
 import { getPoints } from './lib/calc.mjs';
 import { environment } from './lib/env.mjs';
 
 export { beckerhagens };
 export { archimedean };
+export { platonic };
 export { getPoints };
 export { environment };
