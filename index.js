7c5d04282bffb18f271e6ffb285fe26c507fea1a
Joerg Roth <joerg.roth@geo.uzh.ch>
2024-05-01 13:34:21 +0200

    added archimedean und catalan solids

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { beckerhagens, archimedean, platonic } from './lib/solids.mjs';
+import { beckerhagens, archimedean, platonic, catalan } from './lib/solids.mjs';
 import { getPoints } from './lib/calc.mjs';
 import { environment } from './lib/env.mjs';
 
 export { beckerhagens };
+export { catalan };
 export { archimedean };
 export { platonic };
 export { getPoints };
