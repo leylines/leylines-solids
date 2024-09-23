04b9961cf09b4f676efad0a453006a240fab8fec
Joerg Roth <joerg.roth@geo.uzh.ch>
2024-09-23 10:54:24 +0200

    added first portion of dualGeodesics

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ import { environment } from './lib/env.mjs';
 import { platonic } from './lib/platonic.mjs';
 import { archimedean } from './lib/archimedean.mjs';
 import { catalan } from './lib/catalan.mjs';
+import { dualGeodesic } from './lib/dualGeodesic.mjs';
 
 
 export { beckerhagens };
 export { platonic };
 export { archimedean };
 export { catalan };
+export { dualGeodesic };
 export { getPoints };
 //export { environment };
